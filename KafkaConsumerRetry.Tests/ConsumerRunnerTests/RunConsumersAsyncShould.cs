b91f022bcfb1711d28d3b43c7b9ddf1d5f507dff
deeja <deeja@users.noreply.github.com>
2023-01-04 11:16:08 +1300

    Test movement from topic to parition message manager

--- KafkaConsumerRetry.Tests/ConsumerRunnerTests/RunConsumersAsyncShould.cs
+++ KafkaConsumerRetry.Tests/ConsumerRunnerTests/RunConsumersAsyncShould.cs
@@ -54,6 +54,39 @@ public class RunConsumersAsyncShould {
 
         retryConsumer.Verify(consumer1 => consumer1.Subscribe(names.Retries));
     }
+    
+    [Fact(Timeout = 500)]
+    public async Task Move_Consumed_Messages_To_PartitionMessageManager() {
+        var consumerFactory = new Mock<IConsumerFactory>();
+        var messageManager = new Mock<IPartitionMessageManager>();
+        var sut = new ConsumerRunner(consumerFactory.Object, messageManager.Object);
+        var names = new TopicNames("origin", new[] { "retry" }, "dead_letter");
+        var retryConfig = new KafkaRetryConfig();
+        var originConsumer = new Mock<IConsumer<byte[], byte[]>>();
+        var retryConsumer = new Mock<IConsumer<byte[], byte[]>>();
+        var tokenSource = new CancellationTokenSource();
+        var consumeResult = new ConsumeResult<byte[], byte[]>();
+
+        consumerFactory.Setup(factory => factory.BuildOriginConsumer(retryConfig, names)).Returns(originConsumer.Object);
+        consumerFactory.Setup(factory => factory.BuildRetryConsumer(retryConfig, names)).Returns(retryConsumer.Object);
+
+        var tokenSourceToken = tokenSource.Token;
+
+
+        originConsumer.Setup(consumer => consumer.Consume(tokenSourceToken)).Returns(consumeResult);
+        messageManager.Setup(manager => manager.QueueConsumeResult<DummyHandler>(consumeResult)).Callback(() => tokenSource.Cancel());
+
+        try {
+            await sut.RunConsumersAsync<DummyHandler>(retryConfig, names, tokenSourceToken);
+        }
+        catch (TaskCanceledException) {
+            // expected exception
+        }
+        
+        messageManager.Verify(manager => manager.QueueConsumeResult<DummyHandler>(consumeResult));
+
+        
+    }
 }
 
 // ReSharper disable once ClassNeverInstantiated.Global
