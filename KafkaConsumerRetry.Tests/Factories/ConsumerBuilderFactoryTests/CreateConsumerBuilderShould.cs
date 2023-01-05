1541a444e1bc72beeebc2ff06a5f81f2b37fe9c7
deeja <deeja@users.noreply.github.com>
2023-01-06 12:20:24 +1300

    ConsumerBuilderFactory

---
+++ KafkaConsumerRetry.Tests/Factories/ConsumerBuilderFactoryTests/CreateConsumerBuilderShould.cs
@@ -0,0 +1,16 @@
+﻿using System.Reflection;
+using KafkaConsumerRetry.Factories;
+
+namespace KafkaConsumerRetry.Tests.Factories.ConsumerBuilderFactoryTests; 
+
+public class CreateConsumerBuilderShould {
+    [Fact]
+    public void Return_ConsumerBuilder_With_Config_Set() {
+        ConsumerBuilderFactory sut = new ConsumerBuilderFactory();
+        var consumerConfig = new ConsumerConfig();
+        var consumerBuilder = sut.CreateConsumerBuilder(consumerConfig);
+        // Get config via reflection
+        var value = consumerBuilder.GetType().GetProperty("Config",  BindingFlags.NonPublic | BindingFlags.Instance ).GetValue(consumerBuilder)!;
+        value.Should().BeSameAs(consumerConfig);
+    }
+}
